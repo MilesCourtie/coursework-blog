273f82c595aab50ecb985f00dd9bf73b65c5f9ca
Miles Courtie <miles.courtie@gmail.com>
2020-06-24 13:05:10 +0100

    security improvements

--- my_site/settings.py
+++ my_site/settings.py
@@ -20,7 +20,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '-j%d%50-eynrqo*((x^@0e63+(0b(_vd2c5!0pv1ia3_5fkbfg'
+with open(os.path.join(BASE_DIR, 'secret-key.txt')) as f:
+    SECRET_KEY = f.read().strip()
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
